f68dfeff62cfffa47743947caedff109e8c8061c
ZPH <Zander@civet.ws>
2012-10-13 12:34:01 -0400

    Refactoring to merge in changes

--- lib/trello-archiver.rb
+++ lib/trello-archiver.rb
@@ -1,1 +1,1 @@
-require_relative 'trello-archiver/filewriter_xlsx_writer'
+require_relative 'trello-archiver/filewriter'
--- lib/trello-archiver/filewriter.rb
+++ lib/trello-archiver/filewriter.rb
@@ -1,46 +1,119 @@
-def createspreadsheet(board, filenamestub="productbacklogbackup", includecomments=true)
-  # Filenamestub = filename or default of boardname
-  # 
-  # Board object has been passed into the method
-  lists = board.lists
-  FileUtils.mkdir("archive") unless Dir.exists?("archive")
-  filename = "archive/#{DateTime.now.strftime "%Y%m%dT%H%M"}_#{filenamestub}.xlsx"
-  SimpleXlsx::Serializer.new(filename) do |doc|
-    lists.each do |list|
-      # logging puts of list name
-      puts list.name
-      # Gather set of all cards in each list
-      cards = list.cards
-      # Add workbook based on list name
-      doc.add_sheet(list.name) do |sheet|
-        # Add header row
-        sheet.add_row(%w{Name Description Labels Comments})
-        # For each card, dump data in new row
+require 'xlsx_writer'
+
+class TrelloArchiver
+
+  def initialize(options = {:board => "", :filename => "trello_backup", :format => 'xlsx'})
+    @options = options
+    FileUtils.mkdir("archive") unless Dir.exists?("archive")
+  end
+
+  def createspreadsheet()
+    case @options[:format]
+    when 'csv'
+      create_csv
+    when 'xlsx'
+      create_xlsx
+    else
+      create_xlsx
+    end
+    
+  end
+
+  def create_csv()
+    require 'CSV'
+    # Filename= filename or default of boardname
+    # 
+    # Board object has been passed into the method
+    lists = @options[:board].lists
+    filename = "archive/#{DateTime.now.strftime "%Y%m%dT%H%M"}_#{@options[:filename]}.csv"
+
+    CSV.open(filename, "w") do |csv|
+      # Sheets have to be restructured as a field
+      #
+      # Add header row
+      csv << %w[Name Description Labels Progress Comments]
+
+      lists.each do |list|
+        puts list.name
+        cards = list.cards
+        
         cards.each do |card|
-          require 'pry'; binding.pry
-          puts "Title: #{card.name} Desc: #{card.description} List: #{card.list.name} Labels:#{card.labels.length}"
-          
-          if card.labels.length==0 then labellist = "none" else labellist = "" end
-          card.labels.each do |label|
-            labellist += " " + label.name
+          # Add title row
+          puts card.name
+          # gather and join the labels if they exist
+          labels = case card.labels.length
+          when 0
+            "none"
+          else
+            card.labels.map { |c| c.name }.join(" ")
           end
 
-          if includecomments
-            # Gather all items from card's timeline, ie all the comments along the way
-            actions = card.actions
-            commentslist = ""
-            actions.each do |action|
-              if action.type=="commentCard"
-                commentslist += "#{Member.find(action.member_creator_id).full_name} says: #{action.data['text']} \n\n"
-              end
+          # Gather comments
+          comments = card.actions.map do |action|
+            if action.type == "commentCard"
+              # require 'pry'; binding.pry
+              "#{Member.find(action.member_creator_id).full_name} [#{ action.date.strftime('%m/%d/%Y') }] : #{action.data['text']} \n\n"
             end
           end
-          
-          # After each of the content items has been created,
-          # dump that data into a new row
-          sheet.add_row([card.name, card.description, labellist, commentslist])
+
+          csv << [card.name, card.description, labels, list.name, comments.join('')]
         end
       end
     end
   end
+
+  def create_xlsx()
+    # Filename= filename or default of boardname
+    # 
+    # Board object has been passed into the method
+    lists = @options[:board].lists
+    filename = "archive/#{DateTime.now.strftime "%Y%m%dT%H%M"}_#{@options[:filename]}.xlsx"
+
+    @doc = XlsxWriter.new
+
+    lists.each do |list|
+      sheet = @doc.add_sheet(list.name.gsub(/\W+/, '_'))
+      puts list.name
+      cards = list.cards
+      #
+      # Add header row
+      sheet.add_row( %w[Name Description Labels Comments])
+      
+      cards.each do |card|
+        # Add title row
+        puts card.name
+        # sheet.add_row([
+        #   "Title: #{card.name}",
+        #   "Desc: #{card.description}",
+        #   "Labels: #{card.labels.length}"
+        # ])
+        
+        # gather and join the labels if they exist
+        labels = case card.labels.length
+        when 0
+          "none"
+        else
+          card.labels.map { |c| c.name }.join(" ")
+        end
+
+        # Gather comments
+        comments = card.actions.map do |action|
+          if action.type == "commentCard"
+            # require 'pry'; binding.pry
+            "#{Member.find(action.member_creator_id).full_name} [#{ action.date.strftime('%m/%d/%Y') }] : #{action.data['text']} \n\n"
+          end
+        end
+
+        sheet.add_row([card.name, card.description, labels, comments.join('')])
+      end
+    end
+
+    # Moving file to where I want it
+    require 'fileutils'
+    ::FileUtils.mv @doc.path, File.join(File.dirname(__FILE__), "..", "..", filename)
+
+    # Cleanup of temp dir
+    @doc.cleanup
+  end
+
 end
