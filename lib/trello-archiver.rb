7ecdd507c24216b4c4f36c11f22d1516c1071ee3
ZPH <Zander@civet.ws>
2012-12-30 20:36:49 -0500

    Moved require statements to trello-archiver.rb

--- lib/trello-archiver.rb
+++ lib/trello-archiver.rb
@@ -1,3 +1,6 @@
+require 'trello'
+require 'rubygems'
+require 'yaml'
 require_relative 'trello-archiver/authorize'
 require_relative 'trello-archiver/prompt'
 require_relative 'trello-archiver/archiver'
