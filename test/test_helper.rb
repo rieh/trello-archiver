803706a1fc386290d4a446d1d70a8164af52a18e
ZPH <Zander@civet.ws>
2012-12-31 01:03:44 -0500

    Removing files due to switch to Rspec

--- test/test_helper.rb
+++
@@ -1,5 +0,0 @@
-require 'minitest/autorun'
-require 'fakeweb'
-require 'vcr'
-require_relative 'vcr_setup'
-require_relative '../lib/trello-archiver.rb'
--- test/vcr_setup.rb
+++
@@ -1,6 +0,0 @@
-require 'vcr'
-
-VCR.config do |c|
-  c.cassette_library_dir = 'vcr_cassettes'
-  c.stub_with :fakeweb
-end
